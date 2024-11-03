ce186c13cd032698ca4000c073b046d1fcab2298
veera reddy inakollu <ivreddy1422@gmail.com>
2024-11-03 10:56:07 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-sudo apt -y update
-sudo apt-get -y full-upgrade
-sudo apt-get install -y git
-sudo apt-get install -y python3-pip
+sudo yum -y update
+sudo yum install -y git
+sudo yum install -y python3-pip
 git clone https://github.com/veerareddy1422/Medical-Insurance.git
 cd Medical-Insurance/
 pip3 install -r requirements.txt
