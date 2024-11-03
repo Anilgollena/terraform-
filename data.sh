76b56dc459636b947d33216a53950f94314d0e02
veera reddy inakollu <ivreddy1422@gmail.com>
2024-11-03 09:35:23 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-sudo yum -y install git
-sudo yum -y install docker
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo usermod -aG docker ec2-user
-sudo chmod 666 /var/run/docker.sock
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-git clone https://github.com/veerareddy1422/wordpress.git
-cd wordpress/
-docker-compose up -d
+sudo apt -y update
+sudo apt-get -y full-upgrade
+sudo apt-get install -y git
+sudo apt-get install -y python3-pip
+git clone https://github.com/veerareddy1422/Medical-Insurance.git
+cd Medical-Insurance/
+pip3 install -r requirements.txt
+python3 app.py
+
