62071072196107bfcb70c78b939bcb73d0640683
veera reddy inakollu <ivreddy1422@gmail.com>
2024-11-04 16:39:26 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -10,6 +10,12 @@ ingress  {
     cidr_blocks = ["0.0.0.0/0"]
 }
 ingress  {
+    from_port = "8000"
+    to_port = "8000"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+ingress  {
     from_port = 22
     to_port = 22
     protocol = "tcp"
