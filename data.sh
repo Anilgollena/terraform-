1b2a3a0c124aa1b6efdb5b763e77034769a43b63
veera reddy inakollu <ivreddy1422@gmail.com>
2024-11-04 15:50:54 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -2,7 +2,7 @@
 sudo yum -y update
 sudo yum install -y git
 sudo yum install -y python3-pip
-git clone https://github.com/veerareddy1422/Medical-Insurance.git
+git clone https://github.com/veerareddy1422/indian-liver-patients.git
 cd Medical-Insurance/
 pip3 install -r requirements.txt
 python3 app.py
