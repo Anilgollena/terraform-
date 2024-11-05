2bb8ea6611686b6915cf8e8b3c14bb854a6ec740
Anilgollena <115261722+Anilgollena@users.noreply.github.com>
2024-11-05 17:04:19 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "instance-wordpress" {
   ami = "ami-06b21ccaeff8cd686"
   instance_type = "t2.micro"
-  key_name = "veera"
+  key_name = "anil"
   subnet_id = aws_subnet.public_sb_1.id
   vpc_security_group_ids = [aws_security_group.security1.id]
   associate_public_ip_address = true
