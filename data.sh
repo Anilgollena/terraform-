5f741ea7794112ab03e7e85d23db7ee46b9c3ccf
Anilgollena <115261722+Anilgollena@users.noreply.github.com>
2024-11-05 16:50:43 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -3,7 +3,7 @@ sudo yum -y update
 sudo yum install -y git
 sudo yum install -y python3-pip
 git clone https://github.com/veerareddy1422/indian-liver-patients.git
-cd Medical-Insurance/
+cd indian-liver-patients/
 pip3 install -r requirements.txt
 python3 app.py
 
